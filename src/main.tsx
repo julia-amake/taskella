0c00731aaec86e9356ae3e651e8020a180e9a855
Yulia Starchenko <imay@amake.ru>
2025-01-26 22:10:16 +0300

    build: integrate mantine

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,16 @@
-import { StrictMode } from 'react'
-import { createRoot } from 'react-dom/client'
-import './index.css'
-import App from './App.tsx'
+import { createRoot } from 'react-dom/client';
+import './index.css';
+import "@mantine/core/styles.css";
+import "@mantine/dates/styles.css";
+import "@mantine/dropzone/styles.css";
+import "@mantine/notifications/styles.css";
+import "@mantine/spotlight/styles.css";
+import "@mantine/tiptap/styles.css";
+import App from './App.tsx';
+import { MantineProvider } from "@mantine/core";
 
 createRoot(document.getElementById('root')!).render(
-  <StrictMode>
-    <App />
-  </StrictMode>,
-)
+    <MantineProvider>
+        <App/>
+    </MantineProvider>,
+);
