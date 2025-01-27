7f9f6e5eb0300b5a97c1dbd75f573debdce0edb8
Yulia Starchenko <imay@amake.ru>
2025-01-27 12:36:37 +0300

    fix: format code

--- src/main.tsx
+++ src/main.tsx
@@ -1,16 +1,19 @@
 import { createRoot } from 'react-dom/client';
-import './index.css';
-import "@mantine/core/styles.css";
-import "@mantine/dates/styles.css";
-import "@mantine/dropzone/styles.css";
-import "@mantine/notifications/styles.css";
-import "@mantine/spotlight/styles.css";
-import "@mantine/tiptap/styles.css";
+
+import { MantineProvider } from '@mantine/core';
+
+import '@mantine/core/styles.css';
+import '@mantine/dates/styles.css';
+import '@mantine/dropzone/styles.css';
+import '@mantine/notifications/styles.css';
+import '@mantine/spotlight/styles.css';
+import '@mantine/tiptap/styles.css';
+
 import App from './App.tsx';
-import { MantineProvider } from "@mantine/core";
+import './index.css';
 
 createRoot(document.getElementById('root')!).render(
-    <MantineProvider>
-        <App/>
-    </MantineProvider>,
+  <MantineProvider>
+    <App />
+  </MantineProvider>,
 );
